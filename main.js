030445178e73aaaa7ce491b0ff2d36d800a0e7fc
pradeepamurugan <kmrpradeepa123@gmail.com>
2023-02-07 12:38:44 +0530

    update

---
+++ main.js
@@ -0,0 +1,107 @@
+//Addition//
+	var a=5;
+	var b=10;
+	var c=6;
+	var x=a+b+c;
+	document.write(x+'<br>');
+//Sub//
+	var a=10;
+	var b=4;
+	var c=2;
+	var y=a-b-c;
+	document.write(y+'<br>');
+	
+//mult//
+	var a=6;
+	var b=9;
+	var c=10;
+	var z=a*b*c;
+	document.write(z+'<br>');
+	
+//div//
+	var a=2;
+	var b=4;
+	var x=a/b;
+	document.write(x+'<br>');
+//total//
+	var a=8;
+	var b=10;
+	var c=6;
+	var d=2;
+	var x=8+10+6+2;
+	document.write(x+'<br>');
+//Average//
+	var a=8;
+	var b=10;
+	var c=6;
+	var d=2;
+	var	x=26/4;
+	document.write(x+'<br>');
+//simple integer//
+	var p=1000;
+	var t=6;
+	var r=15;
+	var x=p*t*r/100;
+	document.write(x+'<br>');
+//square//
+	var a=4;
+	var x=a*a;
+	document.write(x+'<br>');
+//cube//
+	var a1=8;
+	var y=6*a1*a1;
+	document.write(y+'<br>');
+//area of triangle//
+	var a=10;
+	
+	var b=14;
+	var c=8;
+	var z=32/2;
+	document.write(z+'<br>');
+//circle//
+	var r=6;
+	var s=3.14;
+	var x=s*r*r;
+	document.write(x+'<br>');
+//large//
+	var a=20;
+	var b=8;
+	var x=a>b;
+	document.write(x+'<br>');
+//positive//
+	var num=2;
+	var x=(num>0);
+	document.write(x+'<br>');
+	var num1=-2;
+	var y=(num1>0)
+	document.write(y+'<br>');
+//odd and even//
+	var a=10;
+	var x=(a%2)==0;
+	document.write(y+'<br>');
+	var a=13;
+	var x=(a%2)==0;
+	document.write(x+'<br>');
+//graterthan 10//
+	var a=12;
+	var x=a>10;
+	document.write(x+'<br>');
+	
+	var num=8;
+	var y=num>10;
+	document.write(y+'<br>');
+//divisable by 5//
+	var num=20;
+	var a1=(num%5)==0;
+	document.write(a1+'<br>');
+	var num1=22;
+	var a2=(num1%5)==0;
+	document.write(a2+'<br>');
+
+	
+		
+			
+		
+
+
+	
